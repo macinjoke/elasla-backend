f9505ad05e9945543d18abf17e30321a2bf3fb7f
macinjoke <shunji.makino@gmail.com>
2019-01-13 10:48:42 +0900

    api に移動

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,54 @@
 import express from 'express'
+import { sign, verify as _verify } from 'jsonwebtoken'
+import passport from 'passport'
+import { promisify } from 'util'
 
+const verify = promisify(_verify)
 const router = express.Router()
+
+const secretKey = 'aweiojvcnmcvsadifoweafjewa' // TODO
 
 router.get('/some_data', (req, res, next) => {
   res.json({ some_hoge: { A: 'aaaa', B: 'bbbb' } })
 })
+
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false }),
+  (req, res) => {
+    console.log(req.body)
+    console.log('success authentication')
+    res.json({
+      ...req.body,
+      jwt: sign({ username: req.body.username }, secretKey),
+    })
+  },
+)
+
+router.get('/login', async (req: any, res) => {
+  const jsonWebToken = req.headers.authorization.split(' ')[1]
+  const decoded: any = await verify(jsonWebToken, secretKey).catch(e => {
+    res.status(400).json({ error: 'MalformedJWT' })
+    return
+  })
+  console.log('decoded: ' + JSON.stringify(decoded))
+  if (!decoded) return
+  if (decoded.username === 'test') {
+    res.json({
+      username: 'test',
+      password: 'test',
+    })
+    return
+  }
+  res.status(401).json({ error: 'Unauthorized', detail: 'invalid jwt' })
+})
+
+router.post(
+  '/secure/local',
+  passport.authenticate('local', { session: false }),
+  (req, res) => {
+    res.send('Secure response from ' + JSON.stringify(req.user))
+  },
+)
 
 export default router
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import bodyParser from 'body-parser'
 import express from 'express'
-import { sign, verify as _verify } from 'jsonwebtoken'
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
-import { promisify } from 'util'
 import api from './api'
 
-const verify = promisify(_verify)
 const app = express()
-
-const secretKey = 'aweiojvcnmcvsadifoweafjewa' // TODO
 
 passport.use(
   new LocalStrategy((username, password, done) => {
@@ -36,44 +31,5 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api', api)
-
-app.post(
-  '/api/login',
-  passport.authenticate('local', { session: false }),
-  (req, res) => {
-    console.log(req.body)
-    console.log('success authentication')
-    res.json({
-      ...req.body,
-      jwt: sign({ username: req.body.username }, secretKey),
-    })
-  },
-)
-
-app.get('/api/login', async (req: any, res) => {
-  const jsonWebToken = req.headers.authorization.split(' ')[1]
-  const decoded: any = await verify(jsonWebToken, secretKey).catch(e => {
-    res.status(400).json({ error: 'MalformedJWT' })
-    return
-  })
-  console.log('decoded: ' + JSON.stringify(decoded))
-  if (!decoded) return
-  if (decoded.username === 'test') {
-    res.json({
-      username: 'test',
-      password: 'test',
-    })
-    return
-  }
-  res.status(401).json({ error: 'Unauthorized', detail: 'invalid jwt' })
-})
-
-app.post(
-  '/api/secure/local',
-  passport.authenticate('local', { session: false }),
-  (req, res) => {
-    res.send('Secure response from ' + JSON.stringify(req.user))
-  },
-)
 
 app.listen(3000, () => console.log('Listening on 3000...'))
